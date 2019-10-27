b1026c9370627f0f2af4a9a7103b316693f6ad82
qkmics <qiank@uchicago.edu>
2019-10-27 14:29:19 -0500

    handle comments

---
+++ README.txt
@@ -0,0 +1,21 @@
+How to compile this code:
+          	This cpp file can be compiled in any environment support C++11 and STL. 
+	In Windows: 
+		Build a c++ project in a IDE and then add this c++ file to the project can simply compile it.
+	In Linux: 
+		"g++ remove_comments.cpp" to  get a.out,
+		"./a.out" to run a.out
+		"fileName.in" type in the file name with full path
+		get the filename.out in the same path of the input file
+	This program has been tested in both Windows 10 and Ubuntu 18.04.3
+
+How to run this code:
+	After compiling, run the executable file. Then the programm word prompt user to type in a input file name. Input file should 
+be a txt file. Input file name should include the  full path. (sample: C:\Users\qiankun\Desktop\test.in)
+
+What works and what doesn't work:
+	For all input txt file without a single "\*", the program works. It can not deal with a single "\*" without "*/". 
+	
+	
+ 
+
---
+++ src/remove_comments.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace::std;
+class finiteStateMachine
+{
+private:
+	ifstream inputFile;
+	ofstream outputFile;
+
+public:
+	bool runFSM(string inputFileName, string outputFileName) // use finite automaton
+	{
+		//open the input file and output file
+		inputFile.open(inputFileName);
+		outputFile.open(outputFileName);
+
+		if (inputFile.is_open())
+		{
+			int state = 0;
+			string inputLine;
+			string outputLine;
+
+			while (getline(inputFile, inputLine))  //get a line from the input file
+			{
+				outputLine = "";
+				if (state == 2)       //handle one line comments
+					state = 0;
+				for (char ele : inputLine)
+				{
+					switch (state)     //the automaton has 5 states
+					{
+					case 0:
+						if (ele == '/')						//it might be the start of a commemt
+							state = 1;
+						else if (ele != ' ' && ele != '\t')
+							outputLine.push_back(ele);
+						break;
+
+					case 1:
+						if (ele == '/')						//one line comment
+							state = 2;
+						else if (ele == '*')				//could be a comment
+							state = 3;
+						else
+						{
+							state = 0;
+							outputLine.push_back('/');
+							outputLine.push_back(ele);
+						}
+						break;
+
+					case 2:						//the whole line is an one line comment, so do nothing 
+						break;
+
+					case 3:
+						if (ele == '*')
+							state = 4;
+						break;
+
+					case 4:
+						if (ele == '/')
+							state = 0;
+						else if (ele != '*')
+							state = 3;
+						break;
+					}
+
+				}
+				
+				if (outputLine.size() > 1)	//this input line is not part of a comment
+				{
+					outputFile << outputLine << endl;
+				}
+			}
+		}
+		else
+		{
+			return false;
+		}
+		//close the input file and output file
+		inputFile.close();
+		outputFile.close();
+		return true;
+	}
+
+public:
+};
+int main(void)
+{
+	finiteStateMachine mc;
+
+	string inputName, outputName;
+	cout << "Please input the input file name with the full path: ";
+	getline(cin, inputName);
+
+	// get output file name
+	outputName = inputName;
+	outputName.pop_back();
+	outputName.pop_back();
+	outputName += "out";
+
+	while (!mc.runFSM(inputName, outputName))   //input error handling
+	{
+		cout << "Input file does not exists, please input another file: ";
+		getline(cin, inputName);
+	}
+	cout << "Success" << endl;
+	cout << "Output file is " << outputName << endl;
+	return 0;
+}
